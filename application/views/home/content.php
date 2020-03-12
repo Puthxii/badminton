ebc3d5ce7527d6a096dc0d2b67989e447bef9ee5
puthxii <teenaputh12@gmail.com>
2020-03-12 22:51:43 +0700

    ีcreate input group register competition

--- application/views/home/content.php
+++ application/views/home/content.php
@@ -3,18 +3,79 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Dashboard
-        <small>Control panel </small>
+        Register for the competition
+        <small>walailak university</small>
       </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Dashboard</li>
-      </ol>
     </section>
 
     <!-- Main content -->
     <section class="content">
- 
+      <div class="row">
+        <!-- left column -->
+        <div class="col-md-6">
+          <!-- general form elements -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">สมัครการแข่งขัน</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="box-body">
+                <div class="col-lg-12">
+                  <label for="exampleInputEmail1">ชื่อ สกุล</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="ชื่อ สกุล">
+                </div>
+                <div class="col-lg-6">
+                  <label for="exampleInputEmail1">วัน เดือน ปีเกิด</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="วัน เดือน ปีเกิด">
+                </div>
+                <div class="col-lg-6">
+                  <label for="exampleInputEmail1">อายุ</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="อายุ">
+                </div>       
+                <div class="col-lg-6">
+                  <label for="exampleInputPassword1">ทีม</label>
+                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="ทีม">
+                </div>
+                <div class="col-lg-6">
+                  <label for="exampleInputPassword1">สโมสร</label>
+                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="สโมสร">
+                </div>
+                <div class="col-lg-6">
+                  <label for="exampleInputPassword1">โทร</label>
+                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="โทร">
+                </div>
+                <div class="col-lg-6">
+                  <label for="exampleInputPassword1">line ID</label>
+                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="id">
+                </div>
+                <div class="col-lg-12">
+                  <label>รุ่น</label>
+                  <select class="form-control">
+                    <option>รุ่นเยาวชน</option>
+                    <option>รุ่นประชาชน</option>
+                  </select>
+                </div>
+                <!-- <div class="form-group">
+                  <label for="exampleInputFile">File input</label>
+                  <input type="file" id="exampleInputFile">
+                  <p class="help-block">Example block-level help text here.</p>
+                </div> -->
+                <!-- <div class="checkbox">
+                  <label>
+                    <input type="checkbox"> Check me out
+                  </label>
+                </div> -->
+              </div>
+              <!-- /.box-body -->
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
+        </div>
     </section>
     <!-- /.content -->
   </div>
