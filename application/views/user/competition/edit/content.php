536da0b22e842815a418ba8c2e554d0b6c525872
naruebate27 <naruebate27@gmail.com>
2020-04-16 20:08:36 +0700

    edit content & add script

--- application/views/user/competition/edit/content.php
+++ application/views/user/competition/edit/content.php
@@ -207,6 +207,7 @@
                         </div>
                         <!-- /.box-body -->
                         <div class="box-footer">
+                        <a href="<?php echo base_url('controllers/RegisterCompetition/editCompetition') ?>" type="button" class="btn btn-default">Cancel</a>
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
                     </form>
@@ -217,52 +218,3 @@
     <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
-    <script>
-        $(function() {
-
-            $('#regis-compet').validate({
-                rules: {
-
-                    'P_name': "required",
-                    'P_dob': "required",
-                    'club_name': "required",
-                    'phone_num': "required",
-                    'line': "required",
-                    'Comp_type': "required",
-                    't_name': "required",
-                    'name1': "required",
-                    'nickname1': "required",
-                    'dob1': "required",
-                    'gender1': "required",
-                    'his1': "required",
-                    'name2': "required",
-                    'nickname2': "required",
-                    'dob2': "required",
-                    'gender2': "required",
-                    'his2': "required",
-
-                },
-                messages: {
-
-                    'P_name': "กรอกชื่อผู้สมัครการแข่งขัน",
-                    'P_dob': "กรอก วัน/เดือน/ปี เกิด ผู้สมัครการแข่งขัน",
-                    'club_name': "กรอกชื่อสโมสรผู้สมัครการแข่งขัน",
-                    'phone_num': "กรอกเบอร์โทรผู้สมัครการแข่งขัน",
-                    'line': "กรอกไลน์ผู้สมัครการแข่งขัน",
-                    'Comp_type': "เลือกประเภทการแข่งขัน",
-                    't_name': "กรอกชื่อทีม",
-                    'name1': "กรอกชื่อนักกีฬาคนที่ 1",
-                    'nickname1': "กรอกชื่อเล่นนักกีฬาคนที่ 1",
-                    'dob1': "กรอก วัน/เดือน/ปี เกิด นักกีฬาคนที่ 1",
-                    'gender1': "กรอกเพศนักกีฬาคนที่ 1",
-                    'his1': "กรอกประวัติการแข่งขันนักกีฬาคนที่ 1",
-                    'name2': "กรอกชื่อนักกีฬาคนที่ 2",
-                    'nickname2': "กรอกชื่อเล่นนักกีฬาคนที่ 2",
-                    'dob2': "กรอก วัน/เดือน/ปี เกิด นักกีฬาคนที่ 2",
-                    'gender2': "กรอกเพศนักกีฬาคนที่ 2",
-                    'his2': "กรอกประวัติการแข่งขันนักกีฬาคนที่ 2",
-
-                }
-            });
-
-        });
