141cc3855bc0d84689d1ecc8d0a59e9f20485c14
naruebate27 <naruebate27@gmail.com>
2020-04-16 21:45:42 +0700

    chang lg tournament

--- application/views/admin/tournament/content.php
+++ application/views/admin/tournament/content.php
@@ -20,49 +20,49 @@
                 <ul class="round round-1">
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top winner">Lousville <span>79</span></li>
+                    <li class="game game-top winner">อควาเรียส <span>79</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">NC A&T <span>48</span></li>
+                    <li class="game game-bottom ">อีโวล <span>48</span></li>
 
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top winner">Colo St <span>84</span></li>
+                    <li class="game game-top winner">มิส <span>84</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Missouri <span>72</span></li>
+                    <li class="game game-bottom ">บุรีรัมย์ <span>72</span></li>
 
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top ">Oklahoma St <span>55</span></li>
+                    <li class="game game-top ">พืชสีเขียว <span>55</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom winner">Oregon <span>68</span></li>
+                    <li class="game game-bottom winner">เบยอน <span>68</span></li>
 
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top winner">Saint Louis <span>64</span></li>
+                    <li class="game game-top winner">อามเมอรี่ <span>64</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">New Mexico St <span>44</span></li>
+                    <li class="game game-bottom ">เดเทรต <span>44</span></li>
 
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top winner">Memphis <span>54</span></li>
+                    <li class="game game-top winner">โกลเดน แคท <span>54</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">St Mary's <span>52</span></li>
+                    <li class="game game-bottom ">ลูซิท ดรีม <span>52</span></li>
 
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top winner">Mich St <span>65</span></li>
+                    <li class="game game-top winner">เมอรินมอ <span>65</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Valparaiso <span>54</span></li>
+                    <li class="game game-bottom ">เมดอินไทยแลนด์ <span>54</span></li>
 
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top winner">Creighton <span>67</span></li>
+                    <li class="game game-top winner">เพอเพวมูส <span>67</span></li>
                     <li class="game game-spacer">&nbsp;</li>
                     <li class="game game-bottom ">Cincinnati <span>63</span></li>
 
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top winner">Duke <span>73</span></li>
+                    <li class="game game-top winner">ชาบเปอร์ <span>73</span></li>
                     <li class="game game-spacer">&nbsp;</li>
                     <li class="game game-bottom ">Albany <span>61</span></li>
 
@@ -71,51 +71,51 @@
                 <ul class="round round-2">
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top winner">Lousville <span>82</span></li>
+                    <li class="game game-top winner">อควาเรียส <span>82</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Colo St <span>56</span></li>
+                    <li class="game game-bottom ">มิส <span>56</span></li>
 
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top winner">Oregon <span>74</span></li>
+                    <li class="game game-top winner">เบยอน <span>74</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Saint Louis <span>57</span></li>
+                    <li class="game game-bottom ">อามเมอรี่ <span>57</span></li>
 
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top ">Memphis <span>48</span></li>
+                    <li class="game game-top ">โกเดน แคท <span>48</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom winner">Mich St <span>70</span></li>
+                    <li class="game game-bottom winner">เมอรินมอ <span>70</span></li>
 
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top ">Creighton <span>50</span></li>
+                    <li class="game game-top ">เพอเพวมูส <span>50</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom winner">Duke <span>66</span></li>
+                    <li class="game game-bottom winner">ชาบเปอร์ <span>66</span></li>
 
                     <li class="spacer">&nbsp;</li>
                 </ul>
                 <ul class="round round-3">
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top winner">Lousville <span>77</span></li>
+                    <li class="game game-top winner">อควาเรียส <span>77</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Oregon <span>69</span></li>
+                    <li class="game game-bottom ">เบยอน <span>69</span></li>
 
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top ">Mich St <span>61</span></li>
+                    <li class="game game-top ">เมอรินมอ <span>61</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom winner">Duke <span>71</span></li>
+                    <li class="game game-bottom winner">ชาบเปอร์ <span>71</span></li>
 
                     <li class="spacer">&nbsp;</li>
                 </ul>
                 <ul class="round round-4">
                     <li class="spacer">&nbsp;</li>
                     
-                    <li class="game game-top winner">Lousville <span>85</span></li>
+                    <li class="game game-top winner">อควาเรียส <span>85</span></li>
                     <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Duke <span>63</span></li>
+                    <li class="game game-bottom ">ชาบเปอร์ <span>63</span></li>
                     
                     <li class="spacer">&nbsp;</li>
                 </ul>       
